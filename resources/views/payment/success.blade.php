8e9e7e0bd958a600963fa6b5b52fd60887ed1252
notanut <natasyajosy@gmail.com>
2025-06-01 17:19:00 +0700

    success page

--- resources/views/payment/success.blade.php
+++ resources/views/payment/success.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
+@section('title', 'EzPajak | Pembayaran Berhasil')
 
 @section('content')
-
+<section class="d-flex flex-column justify-content-center align-items-center text-center">
+    <h1 class="text-primary">Hore! Pembayaran Berhasil!</h1>
+    <img src="" alt="">
+    <button>Ke Dashboard Saya</button>
+</section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,5 @@ Route::get('/calculator', function () {
     return view('calculator.index');
 });
 Route::get('/payment/success', function () {
-    return view('payment.sucess');
+    return view('payment.success');
 });
