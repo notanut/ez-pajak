2e32aae43277eeb2adabd427bab90e9b2ab96fb4
notanut <natasyajosy@gmail.com>
2025-06-30 14:30:37 +0700

    migration

---
+++ database/migrations/2025_06_30_072152_create_penggunas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penggunas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('email');
+            $table->string('password');
+            $table->string('jenis_pekerjaan');
+            $table->float('penghasilan_bruto');
+            $table->float('penghasilan_neto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penggunas');
+    }
+};
