d45ce6d6e84c9b07cde843559b303551672a1179
novianaretnoo <novianaaretno@gmail.com>
2025-07-04 22:13:24 +0700

    fix nav & home

--- resources/views/layouts/partials/navbarAfterLogin.blade.php
+++ resources/views/layouts/partials/navbarAfterLogin.blade.php
@@ -8,11 +8,11 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary bg-white">
-    <div class="container px-4">
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-white shadow">
+    <div class="container px-4 shadow">
         <a class="navbar-brand d-flex align-items-center" href="/">
             <img src="{{asset('images/logo.png')}}" width="40" height="40" class="d-inline-block align-middle">
-            <span class="ms-2 align-middle fs-4 fw-bold ezpajak-color text-blue">EZPajak</span>
+            <span class="ms-2 align-middle fs-4 fst-italic">Ez Pajak</span>
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
