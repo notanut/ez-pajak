81c07fcd2205b1550a16e6fce2b9fa3abdc7139c
Iyanmatius <maetyuian@gmail.com>
2025-07-25 13:14:27 +0700

    History

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -5,37 +5,40 @@
     <div class="d-flex flex-column">
         <h1>Riwayat Perhitungan Kamu</h1>
         <p>Hasil perhitungan yang telah kamu input tersimpan dalam list ini. Unduh untuk disimpan menjadi arsip pribadi.</p>
+        
+        <!-- Tabel Riwayat Perhitungan -->
         <div class="row">
             <div class="col">
                 <h5 class="fw-bold">No</h5>
             </div>
             <div class="col">
-                <h5 class="fw-bold">Nama Dokumen</h5>
+                <h5 class="fw-bold">Metode Pembayaran</h5>
             </div>
             <div class="col">
-                <h5 class="fw-bold">Periode</h5>
+                <h5 class="fw-bold">Total</h5>
             </div>
             <div class="col">
                 <h5 class="fw-bold">Aksi</h5>
             </div>
         </div>
 
-        {{-- @foreach ( as )
+        {{-- Menampilkan Riwayat Perhitungan --}}
+        @foreach ($Transaksi as $index => $item)
         <div class="row">
             <div class="col">
-                <h5 class="fw-bold">No</h5>
+                <p>{{ $index + 1 }}</p> <!-- Nomor Urut -->
             </div>
             <div class="col">
-                <h5 class="fw-bold">Nama Dokumen</h5>
+                <p>{{ $item->metode_pembayaran }}</p> <!-- Nama Dokumen -->
             </div>
             <div class="col">
-                <h5 class="fw-bold">Periode</h5>
+                <p>{{ $item->total }}</p> <!-- Periode -->
             </div>
             <div class="col">
-                <h5 class="fw-bold">Aksi</h5>
+                <a href="{{ route('download', $item->id) }}" class="btn btn-primary">Unduh</a> <!-- Aksi untuk Unduh -->
             </div>
         </div>
-        @endforeach --}}
+        @endforeach
     </div>
 </div>
 @endsection
